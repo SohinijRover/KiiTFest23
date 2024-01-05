fb12be36adc9c4a8678de2dffc691dd7dc504011
Revenant <dasdev.pratik@gmail.com>
2024-01-05 16:02:13 +0530

    🚀 Inclusion of KIIT fest logo

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -12,7 +12,19 @@ const HeroSection = ({ isMenuShown }) => {
         className="absolute inset-0 w-full h-full object-cover"
       />
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-65">
-        <Image
+      <Image 
+          src="/logo svg.png"
+          width={700} 
+          height={108}
+          priority={true}
+          quality={30}
+          alt="Coming Soon Image"
+          style={{
+            margin: "0 auto", 
+            marginTop: "-5px", 
+          }}
+        />
+        <Image 
           src="/files/coming-soon.png"
           width={700} 
           height={108}
